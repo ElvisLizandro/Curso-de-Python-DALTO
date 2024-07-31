0a2a121c79f9f06580d1f54d68b8f744c03ec298
ElvisLizandro <elvislizandrotapia@gmail.com>
2024-07-31 16:53:13 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Definiendo una variable con camelCase
+nombreCompletoDeTuTioMaster = 'Lucas'
+
+# Definiendo una variable con snake_case
+nombre_completo_de_tu_tio_master = 'Lucas'
+
+# Definiendo una variable
+nombre = 'Lucas'
+
+# Concatenar con +
+bienvenida = f"Hola " + " ¿como estas?"
+
+# Concatenar con f-string
+bienvenida = f"Hola {nombre_completo_de_tu_tio_master} ¿como estas?"
+
+# Operadores de pertenencia (in / not in)
+
+print("Lucas" in bienvenida)
+
+print("Lucas" not in bienvenida)
